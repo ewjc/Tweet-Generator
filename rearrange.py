0e3c041ec209fc6f254d5d7069633e8e94b65d53
ewjc <ericswong7@gmail.com>
2017-01-26 14:40:02 -0800

    challange one complete

---
+++ rearrange.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+arg = sys.argv
+del arg[0]
+arg_length = len(arg) - 1
+
+def randomize_words():
+    rand_index = random.randint(0, arg_length)
+    for i in (0, arg_length):
+        temp = arg[i]
+        arg[i] = arg[rand_index]
+        temp = arg[rand_index]
+    return arg
+
+if __name__ == '__main__':
+    words = randomize_words()
+    print(words)
