e61289d5eb71412e9124fb5ec575e59e10741966
ewjc <ericswong7@gmail.com>
2017-01-30 11:05:32 -0800

    oop challange first 2 problems solved

---
+++ OOPchallange.py
@@ -0,0 +1,19 @@
+OOP Challange!
+
+Problem 1:
+def sleep(name):
+    print('{name} sleeps for 8 hours').format(name=name)
+
+Problem 2:
+def eat(name, food):
+    print('{name} eats {food}').format(name=name, food=food)
+    if food == favoriteFood:
+        print('YUM! {name} wants more {food}').format(name=name, food=food)
+
+Problem 3:
+
+Problem 4:
+Problem 5:
+Problem 6:
+Problem 7:
+Problem 8:
