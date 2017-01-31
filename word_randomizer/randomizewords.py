17983b6024ece856a3b063fa2dd4977a856a9e1c
ewjc <ericswong7@gmail.com>
2017-01-31 15:11:51 -0800

    page 2 solved

--- word_randomizer/randomizewords.py
+++ word_randomizer/randomizewords.py
@@ -5,7 +5,11 @@ arg = sys.argv[1]
 
 words_file = open('/usr/share/dict/words', 'r')
 words_list = words_file.readlines()
+message = ''
 
 for index in range(1, int(arg)):
     rand_index = random.randint(0, len(words_list))
-    print(words_list[rand_index])
+    word = words_list[rand_index]
+    message += word.replace('\n', ' ')
+
+print(message)
