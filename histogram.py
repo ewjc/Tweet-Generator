cd2529834edfbef57da080d8beaaefc61703252d
ewjc <ericswong7@gmail.com>
2017-02-01 11:58:20 -0800

    updated histogram func

--- histogram.py
+++ histogram.py
@@ -10,4 +10,10 @@ story_list = story.split()
 def trim_punctuation(story_list):
     story_list = [''.join(c for c in s if c not in string.punctuation) for s in story_list]
     story_list = [s for s in story_list if s]
-    print(story_list)
+
+def histogram():
+    trim_punctuation(story_list)
+    tuple_of_unique_words = ()
+    print(len(story_list))
+    # for word in range(0, len(story_list))
+histogram()
