26888788bd34f873bceae72670996b3de50cc7e7
ewjc <ericswong7@gmail.com>
2017-02-01 12:20:57 -0800

    histogram function writte

--- histogram.py
+++ histogram.py
@@ -1,11 +1,14 @@
 import urllib
 import string
+import sys
 
+arg = sys.argv[1:]
 link = 'http://www.gutenberg.org/cache/epub/10/pg10.txt'
 
 story_file = urllib.urlopen(link)
 story = story_file.read()
 story_list = story.split()
+dict_unique_words = {}
 
 def trim_punctuation(story_list):
     story_list = [''.join(c for c in s if c not in string.punctuation) for s in story_list]
@@ -13,7 +16,10 @@ def trim_punctuation(story_list):
 
 def histogram():
     trim_punctuation(story_list)
-    tuple_of_unique_words = ()
-    print(len(story_list))
-    # for word in range(0, len(story_list))
+    for index_story_list in range(0, len(story_list)):
+        stored_word = story_list[index_story_list]
+        if stored_word not in dict_unique_words:
+            dict_unique_words.add(stored_word=1)
+        elif stored_word in dict_unique_words:
+            dict_unique_words[stored_word] += 1
 histogram()
