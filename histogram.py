3dcdc8d1812319c8fe18ec56ac4b4e6e58514bbd
ewjc <ericswong7@gmail.com>
2017-02-01 13:32:30 -0800

    page 3 completed

--- histogram.py
+++ histogram.py
@@ -2,13 +2,13 @@ import urllib
 import string
 import sys
 
-arg = sys.argv[1:]
 link = 'http://www.gutenberg.org/cache/epub/10/pg10.txt'
 
+arg = sys.argv[1]
 story_file = urllib.urlopen(link)
 story = story_file.read()
 story_list = story.split()
-dict_unique_words = {}
+dict_unique_words = dict()
 
 def trim_punctuation(story_list):
     story_list = [''.join(c for c in s if c not in string.punctuation) for s in story_list]
@@ -19,7 +19,20 @@ def histogram():
     for index_story_list in range(0, len(story_list)):
         stored_word = story_list[index_story_list]
         if stored_word not in dict_unique_words:
-            dict_unique_words.add(stored_word=1)
+            dict_unique_words.update({stored_word : 1})
         elif stored_word in dict_unique_words:
             dict_unique_words[stored_word] += 1
-histogram()
+
+def unique_words_count():
+    print(len(dict_unique_words))
+
+def frequency():
+    if arg in dict_unique_words:
+        print('Frequency of word: ' + str(dict_unique_words[arg]))
+    elif arg not in dict_unique_words:
+        print('Frequency of word: 0')
+
+if __name__ == '__main__':
+    histogram()
+    print('There are a total of {unique_words_count} words.'.format(unique_words_count=len(dict_unique_words)))
+    frequency()
