ec46725942f0880cd9c9a2378efd2796888aba25
ewjc <ericswong7@gmail.com>
2017-02-04 16:16:16 -0800

    randomize word function working

--- word_randomizer/histogram_stochastic.py
+++ word_randomizer/histogram_stochastic.py
@@ -4,15 +4,15 @@ import urllib
 
 link = 'http://www.gutenberg.org/cache/epub/10/pg10.txt'
 
-user_input = sys.argv[1:]
+user_input = sys.argv[1]
 text_file = urllib.urlopen(link)
 story_list = text_file.read()
 story = story_list.split()
 
 def return_random_word(story):
-    rand_index = random.randint(0, len(story))
-    word = story[rand_index]
-    for index in range(0, 5):
+    for index in range(0, int(user_input)):
+        rand_index = random.randint(0, len(story))
+        word = story[rand_index]
         print(word)
 
 return_random_word(story)
